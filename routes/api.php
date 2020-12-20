dbbe4eef9b0f886b48f11204633f47e1466995bb
mtiendat <73454933+mtiendat@users.noreply.github.com>
2020-12-20 12:41:59 +0700

    sua

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('bai-viet','admin\BaiVietController@layDanhSach');
-<<<<<<< Updated upstream
 Route::get('chitiet','admin\BaiVietController@layBaiVietID');
 Route::get('binhluan','admin\BaiVietController@layBinhLuan');
-=======
 Route::get('login','admin\UserController@layDanhSach');
->>>>>>> Stashed changes
